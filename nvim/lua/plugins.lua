492448ff789165e0a15ec1a3f97d21fa78c553e1
Sean O'Hara <sohara@sohara.com>
2022-08-30 18:26:09 -0400

    Intial commit setting up Neovim

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,32 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'L3MON4D3/LuaSnip' -- Snippets
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvimp cpm source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvimp cpm source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'neovim/nvim-lspconfig' -- LSP
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+end)
