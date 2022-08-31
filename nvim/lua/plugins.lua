a448807bce2f49e9ff2de7a4bd85ae36242a1e8f
Sean O'Hara <sohara@sohara.com>
2022-08-31 10:42:02 -0400

    Add mason, mason-lspconfig for tailwindcss support

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -27,6 +27,8 @@ packer.startup(function(use)
   }
   use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
   use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
 
 
   use 'windwp/nvim-autopairs'
