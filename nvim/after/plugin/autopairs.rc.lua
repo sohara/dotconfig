e42b9e84c062289393e3369dc1204c203f249f79
Sean O'Hara <sohara@sohara.com>
2022-09-01 15:06:24 -0400

    Move configs out of `after`, get eslint/prettier working

--- nvim/after/plugin/autopairs.rc.lua
+++ nvim/after/plugin/autopairs.rc.lua
@@ -1,5 +1,5 @@
-local status, autopairs = pcall(require, 'nviim-autopairs')
-if (not status) then return end
+local status, autopairs = pcall(require, 'nvim-autopairs')
+if (not status) then print('!!! Autopairs not working') return end
 
 autopairs.setup {
   disable_filetype = { 'TelescopePrompt', 'vim' }
--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,4 +1,6 @@
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+local set = vim.api.nvim_set_keymap
 
 -- Do not yank with x
 keymap.set('n', 'x', '"_x')
@@ -36,3 +38,11 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- Insert Mode --
+keymap.set('i', 'jk', '<ESC>', opts)
+
+-- Visual Mode --
+-- Move text up and down
+set("v", "<C-j>", ":m .+1<CR>==", opts)
+set("v", "<C-k>", ":m .-2<CR>==", opts)
