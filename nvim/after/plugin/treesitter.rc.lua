f244df70bc5d3363aa44a755d9995a5eb1bec660
Sean O'Hara <sohara@sohara.com>
2022-09-25 11:21:36 -0400

    Add nvim-tree config, format more files

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -1,28 +1,31 @@
-local status, ts = pcall(require, 'nvim-treesitter.configs')
-if (not status) then return end
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if not status then
+	print("Could not start treesitter")
+	return
+end
 
-ts.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    'javascript',
-    'typescript',
-    'tsx',
-    'lua',
-    'json',
-    'css'
-  },
-  autotag = {
-    enable = true
-  },
-  context_commentstring = {
-    enable = true,
-    enabe_autocmd = false
-  }
-}
+ts.setup({
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	ensure_installed = {
+		"javascript",
+		"typescript",
+		"tsx",
+		"lua",
+		"json",
+		"css",
+	},
+	autotag = {
+		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+		enabe_autocmd = false,
+	},
+})
