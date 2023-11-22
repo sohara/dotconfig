901c14a2da536e35a34278a900457a7de525308a
Sean O'Hara <sohara@sohara.com>
2023-11-22 13:48:47 -0500

    Add custom dashboard logo

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,13 +1,34 @@
 return {
-  "folke/noice.nvim",
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      filter = {
-        event = "notify",
-        find = "No information available",
-      },
-      opts = { skip = true },
-    })
-    opts.presets.lsp_doc_border = true
-  end,
+  -- messages, cmdline and the popupmenu
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  -- custom dashboard logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+███████╗ ██████╗ ██╗  ██╗ █████╗ ██████╗  █████╗ 
+██╔════╝██╔═══██╗██║  ██║██╔══██╗██╔══██╗██╔══██╗
+███████╗██║   ██║███████║███████║██████╔╝███████║
+╚════██║██║   ██║██╔══██║██╔══██║██╔══██╗██╔══██║
+███████║╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║
+╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
 }
