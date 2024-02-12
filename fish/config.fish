f08067dd71bd6e08aed0dfbef68555c34cb0ef98
Sean O'Hara <sohara@sohara.com>
2024-02-12 17:08:54 -0500

    Add `g` shortcut func for git

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,6 @@ set -g theme_hide_hostname yes
 # set -g theme_hostname always
 
 # aliases
-alias g git
 alias vim nvim
 
 # PATH
@@ -40,3 +39,14 @@ set -gx PATH "$BUN_INSTALL/bin" $PATH
 # Volta (Node.js version manager)
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Conda/mambaforge
+set -gx PATH ~/mambaforge/bin $PATH
+
+function g
+    if count $argv >/dev/null
+        git $argv
+    else
+        git status
+    end
+end
