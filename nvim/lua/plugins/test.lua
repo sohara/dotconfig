11954d512f5a48ae2163c364a12cf7e2f3c5dca1
Sean O'Hara <sohara@sohara.com>
2024-08-26 14:01:57 -0400

    Updates and add neo-test support

---
+++ nvim/lua/plugins/test.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "haydenmeade/neotest-jest",
+      "marilari88/neotest-vitest",
+      "olimorris/neotest-rspec",
+    },
+    keys = {
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last Test",
+      },
+      {
+        "<leader>tL",
+        function()
+          require("neotest").run.run_last({ strategy = "dap" })
+        end,
+        desc = "Debug Last Test",
+      },
+      {
+        "<leader>tw",
+        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+        desc = "Run Watch",
+      },
+    },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+      table.insert(opts.adapters, require("neotest-vitest"))
+      table.insert(
+        opts.adapters,
+        require("neotest-rspec")({
+          rspec_cmd = function()
+            return vim.tbl_flatten({
+              "docker-compose",
+              "exec",
+              -- "-i",
+              -- "-w",
+              -- "/app",
+              -- "-e",
+              -- "RAILS_ENV=test",
+              -- "app",
+              -- "bundle",
+              -- "exec",
+              "bin/rspec",
+            })
+          end,
+
+          transform_spec_path = function(path)
+            local prefix = require("neotest-rspec").root(path)
+            return string.sub(path, string.len(prefix) + 2, -1)
+          end,
+
+          results_path = "tmp/rspec.output",
+        })
+      )
+    end,
+  },
+}
